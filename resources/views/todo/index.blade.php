f5f4ef37a9de610cae9ce0829795d0785b30309c
nuris akbar <nuris.akbar@gmail.com>
2018-04-21 16:41:46 +0700

    give style table boostrap

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -1,13 +1,30 @@
 @extends('layout')
 @section('title','List Todo')
 @section('content')
-    <h2>List Todo</h2>
-    <ul>
-    @foreach($todos as $todo)
-        <li>{{ $todo }}</li>
-    @endforeach
-    </ul>
+<div class="box">
+        <div class="box-header with-border">
+          <h3 class="box-title">Bordered Table</h3>
+        </div>
+        <div class="box-body">
+            <table class="table table-bordered">
+                <tr>
+                    <th>Title</th>
+                    <th>Description</th>
+                </tr>
+                @foreach($todos as $todo)
+                    <tr>
+                        <td>{{ $todo }}</td>
+                        <td>sample text</td>
+                        <td>password</td>
+                    </tr>
+                @endforeach
+            </table>
+            <hr>
+            <a href="/todo/create" class="btn btn-danger btn-sm">Create New Todo</a>
+        </div>
+</div>
 
- <a href="/todo/create">Create New Todo</a>
+
+
     
 @endsection()
